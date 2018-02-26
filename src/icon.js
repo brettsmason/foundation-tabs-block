7e1e318d1c8e4e8d33c56fc3ca56799815649131
Brett Mason <brettsmason@gmail.com>
2018-02-26 11:54:53 +0000

    Icon rework

--- src/icon.js
+++ src/icon.js
@@ -1,5 +1,5 @@
 const icon = <svg x="0px" y="0px" width="20px" height="20px" viewBox="0 0 20 20">
-<path d="M0,0v20h20V0H0z M12,2v4H8V2H12z M18,18H2V2h4v6h6h6V18z M18,6h-4V2h4V6z"/>
+<path d="M0,0v18h18V0H0z M11,2v3H8V2H11z M16,16H2V2h4v5h5h5V16z M16,5h-3V2h3V5z"/>
 </svg>;
 
 export default icon;
